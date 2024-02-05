174ca71afe2c21796ce4e098017854e7df549854
sungwoo <sfefsfef@gmail.com>
2024-02-05 16:31:38 +0900

    240205_employeeDao

---
+++ GostCompany/dao/EmployeeDao.java
@@ -0,0 +1,28 @@
+package GostCompany.dao;
+
+public class EmployeeDao extends BaseDao {
+    
+
+    //사원 등록 
+    public void join(){
+
+    }
+
+
+
+    // 사원 정보 수정 
+    public void modify(){
+
+    }
+
+
+    //사원 삭제 
+    public void delete(){
+        
+    }
+
+
+
+
+}
+
